e6a81c7e70137eca24c57c9326927c93513f3a97
Gabriel Vallejo <gabrielvallejo2000@gmail.com>
2023-01-31 13:18:24 -0600

    Cambio index

---
+++ app.js
@@ -0,0 +1,4 @@
+ // Con promp ejecutamos una alerta con un input de texto
+const nombre = prompt("¿Como te llamas?");
+// Imprimiremos Un Hola y seguido el nombre del usuario
+alert(`Hola ${nombre}`);
